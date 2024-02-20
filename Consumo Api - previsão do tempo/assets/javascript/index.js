c29a5b58cc45cae5dc9857ff271f60cbc1924184
Marcelo Macedo Lima <159561327+Marcelok200@users.noreply.github.com>
2024-02-20 15:23:33 -0300

    Foi incluido o tratamento de erro

--- Consumo Api - previsão do tempo/assets/javascript/index.js
+++ Consumo Api - previsão do tempo/assets/javascript/index.js
@@ -14,9 +14,17 @@ function envioCep() {
     const urlCep = `http://api.openweathermap.org/geo/1.0/zip?zip=${inputCep},BR&lang=pt_br&appid=8c8e46eab9f9c59522aaef7e6d5773b0`;
 
     return fetch(urlCep)
-    .then(response => response.json())
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Erro ao buscar dados do CEP');
+        }
+        return response.json();
+    })
     .then (respostaJson =>{
         return respostaJson;
+    })
+    .catch(error => {
+        console.error('Erro ao buscar dados do CEP:', error);
     });
 }
 
@@ -28,12 +36,15 @@ function temperatura(){
         const urlTemperatura = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&appid=8c8e46eab9f9c59522aaef7e6d5773b0&units=metric`;
 
         fetch(urlTemperatura)
-        .then(response => response.json())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Erro ao buscar dados de temperatura');
+            }
+            return response.json();
+        })
         .then(temperatura => {
-            console.log(temperatura)
             const resultadoPrevisao = containerPrincipal.querySelector('.resultadoPrevisao');
             const weather = [...temperatura.weather]
-            console.log(weather)
             resultadoPrevisao.innerHTML = `
                 <div class="resultadosTemperatura">
                     <p>Temp. Atual: ${temperatura.main.temp}°C</p>
@@ -41,18 +52,19 @@ function temperatura(){
                     <p>Temp. Mínima: ${temperatura.main.temp_min}°C</p>
                 </div>
                 
-                <h2>Clima atual</h2>
+                <h2>Clima atual na cidade de ${cidade}</h2>
 
                 <div class = "clima">
                     <p>${weather[0].description}</p>
                     <img src = "https://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt = "Foto do tipo climatico">
                 
-                </div>
-
-                
-                
-                `;
-            console.log(cidade);
+                </div>`;
+        })
+        .catch(error => {
+            console.error('Erro ao buscar dados de temperatura:', error);
         });
+    })
+    .catch(error => {
+        console.error('Erro ao buscar dados do CEP:', error);
     });
 }
