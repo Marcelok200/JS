cb6bd395f0ee879c69cd0e331b1c04681624e2ba
Marcelo Macedo Lima <159561327+Marcelok200@users.noreply.github.com>
2024-02-20 11:54:27 -0300

    Concluido

--- Consumo Api - previsão do tempo/assets/javascript/index.js
+++ Consumo Api - previsão do tempo/assets/javascript/index.js
@@ -2,16 +2,16 @@ const containerPrincipal = document.querySelector('.container');
 const formulario = containerPrincipal.querySelector('#formulario');
 const btnEnvio = formulario.querySelector('#btnEnvio');
 
-formulario.addEventListener('submit', previnirEvento);
+formulario.addEventListener('submit', prevenirEvento);
 
-function previnirEvento(e){
+function prevenirEvento(e){
     e.preventDefault();
     temperatura();
 }
 
 function envioCep() {
     const inputCep = formulario.querySelector('#cep').value.replace(/\D+/g, "");
-    const urlCep = `http://api.openweathermap.org/geo/1.0/zip?zip=${inputCep},BR&appid=8c8e46eab9f9c59522aaef7e6d5773b0`;
+    const urlCep = `http://api.openweathermap.org/geo/1.0/zip?zip=${inputCep},BR&lang=pt_br&appid=8c8e46eab9f9c59522aaef7e6d5773b0`;
 
     return fetch(urlCep)
     .then(response => response.json())
@@ -24,12 +24,35 @@ function temperatura(){
     envioCep().then(cep => {
         const lat = cep.lat;
         const lon = cep.lon;
-        const cidade = cep.name
-        const urlTemperatura = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=8c8e46eab9f9c59522aaef7e6d5773b0`;
+        const cidade = cep.name;
+        const urlTemperatura = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&lang=pt_br&appid=8c8e46eab9f9c59522aaef7e6d5773b0&units=metric`;
 
         fetch(urlTemperatura)
         .then(response => response.json())
-        .then(temperatura => console.log(temperatura));
-        console.log(cidade);
+        .then(temperatura => {
+            console.log(temperatura)
+            const resultadoPrevisao = containerPrincipal.querySelector('.resultadoPrevisao');
+            const weather = [...temperatura.weather]
+            console.log(weather)
+            resultadoPrevisao.innerHTML = `
+                <div class="resultadosTemperatura">
+                    <p>Temp. Atual: ${temperatura.main.temp}°C</p>
+                    <p>Temp. Máxima: ${temperatura.main.temp_max}°C</p>
+                    <p>Temp. Mínima: ${temperatura.main.temp_min}°C</p>
+                </div>
+                
+                <h2>Clima atual</h2>
+
+                <div class = "clima">
+                    <p>${weather[0].description}</p>
+                    <img src = "https://openweathermap.org/img/wn/${weather[0].icon}@2x.png" alt = "Foto do tipo climatico">
+                
+                </div>
+
+                
+                
+                `;
+            console.log(cidade);
+        });
     });
 }
